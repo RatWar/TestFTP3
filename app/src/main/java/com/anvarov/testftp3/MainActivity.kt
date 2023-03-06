b093fbc53d1a438db2774d14714005d449ab9a0e
Rais <8Ud-9xV-t75-VBq>
2023-03-06 15:29:41 +0500

    Step 2

--- app/src/main/java/com/anvarov/testftp3/MainActivity.kt
+++ app/src/main/java/com/anvarov/testftp3/MainActivity.kt
@@ -2,10 +2,25 @@ package com.anvarov.testftp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val infoos = findViewById<TextView>(R.id.infoos)
+        var t1=FTPthread()
+        @Suppress("DEPRECATION") val path = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_DOWNLOADS)
+        val fileName = "deleted.json"
+        val fileWrite = File(path, fileName)
+        t1.urisArr = fileWrite
+        t1.num = 1
+        t1.infoos = infoos
+        t1.start()
     }
 }
